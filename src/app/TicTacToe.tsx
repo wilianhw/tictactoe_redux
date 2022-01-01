bd4fce4525859bed36a838019cad0d1210489395
wilian.souza <wiliansouza.ti@fundecc.org.br>
2022-01-01 13:18:16 -0300

    :construction: Estrutura inicial.

---
+++ src/app/TicTacToe.tsx
@@ -0,0 +1,28 @@
+export function TicTacToe() {
+  return (
+    <div className="ticTacToe">
+      <div>Aguardando jogada de X</div>
+
+      <table>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+      <button>Reiniciar partida</button>
+    </div>
+  )
+}
